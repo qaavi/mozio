e625e291fb5ff80ad710df5ab5b61894df7a373c
Abdul Qavi <abdul.qavi@teo-intl.com>
2019-07-21 23:31:34 +0500

    Static files

--- mozio/settings.py
+++ mozio/settings.py
@@ -125,9 +125,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
