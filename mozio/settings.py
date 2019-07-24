f5c71aa7635584c2218ae3c6bf6cd988200a069e
Abdul Qavi <abdul.qavi@teo-intl.com>
2019-07-24 11:26:38 +0500

    whitenoise added

--- mozio/settings.py
+++ mozio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bm=ua@u!s@z$_d5x)d*^@78niqt)jfh3jf+f=^rgcre&3w=6ej'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['159.89.162.71', 'localhost']
 
